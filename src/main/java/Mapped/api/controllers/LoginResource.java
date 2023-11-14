239014f91f7ec1a92f1143b565d5d174a267bb15
AlbertoSeiji <alberto.assatoalberto@gmail.com>
2023-11-14 00:20:36 -0300

    att

--- src/main/java/Mapped/api/controllers/LoginResource.java
+++ src/main/java/Mapped/api/controllers/LoginResource.java
@@ -65,13 +65,5 @@ public class LoginResource {
             repository.delete(id);
         }
     }
-    @OPTIONS
-    public Response options() {
-        return Response.ok()
-                .header("Access-Control-Allow-Origin", "http://localhost:3000")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .build();
-    }
+
 }
--- src/main/java/Mapped/api/infrastructure/http/CorsFilter.java
+++ src/main/java/Mapped/api/infrastructure/http/CorsFilter.java
@@ -3,15 +3,16 @@ package Mapped.api.infrastructure.http;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-//import jakarta.ws.rs.ext.Provider;
+import jakarta.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
-//@Provider
+import jakarta.ws.rs.ext.Provider;
+@Provider
+
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-//        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
 
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
